2f77f9e64b53b543284681b9af929a933010428c
youn0592 <youn0592@algonquinlive.com>
2024-06-26 13:50:46 -0400

    Added basic logic for calculator, plenty more needed to be done

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -15,45 +15,16 @@ public class ButtonManager : MonoBehaviour
     public void NumberButtonPressed(int num)
     {
         CalcManager.InputValue(num);
-        Debug.Log(num);
     }
 
     public void OperationButtonPressed(int opp)
     {
-        switch(opp)
-        {
-            case 0: //Addition
-                CalcManager.InputOpperation(ECalcButton.Addition);
-                break;
-            case 1: //Subtraction
-                CalcManager.InputOpperation(ECalcButton.Subtraction);
-                break;
-            case 2: //Multiplication
-                CalcManager.InputOpperation(ECalcButton.Multiplication);
-                break;
-            case 3: //Division
-                CalcManager.InputOpperation(ECalcButton.Division);
-                break;
-            case 4: //SquareRoot
-                CalcManager.InputOpperation(ECalcButton.SquareRoot);
-                break;
-            case 5: //Exponent
-                CalcManager.InputOpperation(ECalcButton.Exponent);
-                break;
-            case 6: //Percent
-                CalcManager.InputOpperation(ECalcButton.Percent);
-                break;
-            case 7: //Equals
-                CalcManager.InputOpperation(ECalcButton.Equals);
-                break;
-            case 8: //Clear
-                CalcManager.InputOpperation(ECalcButton.Clear);
-                break;
-            case 9: //Deciaml
-                CalcManager.InputOpperation(ECalcButton.Decimal);
-                break;
+        /*Need to pass opperation to CalcManager, however can't take Enum as a variable in Unity Editor.
+         *Enum isn't read as a proper int, but as an system.int32 which isn't declarable in code.
+         *Upcast seems to be a reliable option.
+         * */
 
-        }
+        CalcManager.InputOpperation(opp);
             
     }
 }
--- Assets/Scripts/CalculatorManager.cs
+++ Assets/Scripts/CalculatorManager.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UIElements;
 using System;
+using System.Numerics;
+using System.Diagnostics;
+using UnityEditor.UIElements;
 
 [Serializable]
 //Numbers added for UI OnClick
 public enum ECalcButton
-    {
-        Addition = 0,
-        Subtraction = 1,
-        Multiplication = 2,
-        Division = 3,
-        SquareRoot = 4,
-        Exponent = 5,
-        Percent = 6,
-        Equals = 7,
-        Clear = 8,
-        Decimal = 9
-    }
+{
+    Addition = 0,
+    Subtraction = 1,
+    Multiplication = 2,
+    Division = 3,
+    SquareRoot = 4,
+    Exponent = 5,
+    Percent = 6,
+    Equals = 7,
+    Clear = 8,
+    Decimal = 9
+}
 public class CalculatorManager : MonoBehaviour
 {
     [SerializeField]
@@ -27,8 +30,15 @@ public class CalculatorManager : MonoBehaviour
 
     TextMeshProUGUI outputText;
 
+    //the Current Number being inputed
     double currentNum = 0.0f;
+    //The current sum being calcuated.
     double sum = 0.0f;
+
+    List<double> NumList = new List<double>();
+    List<ECalcButton> OppList = new List<ECalcButton>();
+
+    bool bBEDMAS = false;
 
 
     // Start is called before the first frame update
@@ -36,7 +46,7 @@ public class CalculatorManager : MonoBehaviour
     {
         if (outputObj == null)
         {
-            Debug.LogError("OUTPUT OBJECT IS NULL IN CALCULATOR MANAGER!");
+            UnityEngine.Debug.LogError("OUTPUT OBJECT IS NULL IN CALCULATOR MANAGER!");
         }
 
         outputText = outputObj.GetComponent<TextMeshProUGUI>();
@@ -64,9 +74,133 @@ public class CalculatorManager : MonoBehaviour
         UpdateUI(false);
     }
 
-    public void InputOpperation(ECalcButton opp)
+    public void InputOpperation(int opp)
     {
-        Debug.Log(opp);
+        switch (opp)
+        {
+            case 0: //Addition
+                OppList.Add(ECalcButton.Addition);
+                AdditionCalc();
+                break;
+            case 1: //Subtraction
+                OppList.Add(ECalcButton.Subtraction);
+                SubtractionCalc();
+                break;
+            case 2: //Multiplication
+                OppList.Add(ECalcButton.Multiplication);
+                MultiplacationCalc();
+                break;
+            case 3: //Division
+                OppList.Add(ECalcButton.Division);
+                DivisionCalc();
+                break;
+            case 4: //SquareRoot
+                OppList.Add(ECalcButton.SquareRoot);
+                break;
+            case 5: //Exponent
+                OppList.Add(ECalcButton.Exponent);
+                break;
+            case 6: //Percent
+                OppList.Add(ECalcButton.Percent);
+                break;
+            case 7: //Equals
+                EqualEquation();
+                break;
+            case 8: //Clear
+                NumList.Clear();
+                OppList.Clear();
+                break;
+            case 9: //Deciaml
+                //TODO add Decimal calcuations
+                break;
+        }
+    }
+
+    void AdditionCalc()
+    {
+        //Works, however needs to be changed, is current working on the upcoming calculation.
+        //Needs to be changed to have the previous number be added.
+
+        NumList.Add(currentNum);
+        sum += currentNum;
+        currentNum = 0.0f;
+
+        if(NumList.Count < 1)
+        {
+            UpdateUI(false);
+            return;
+        }
+
+        UpdateUI(true);
+    }
+
+    void SubtractionCalc()
+    {
+        NumList.Add(currentNum);
+        sum -= currentNum;
+        currentNum = 0.0f;
+
+        if (NumList.Count < 1)
+        {
+            UpdateUI(false);
+            return;
+        }
+
+        UpdateUI(true);
+    }
+
+    void MultiplacationCalc()
+    {
+        NumList.Add(currentNum);
+        sum *= currentNum;
+        currentNum = 0.0f;
+
+        if (NumList.Count < 1)
+        {
+            UpdateUI(false);
+            return;
+        }
+
+        UpdateUI(true);
+    }
+
+    void DivisionCalc()
+    {
+        NumList.Add(currentNum);
+        sum /= currentNum;
+        currentNum = 0.0f;
+
+        if (NumList.Count < 1)
+        {
+            UpdateUI(false);
+            return;
+        }
+
+        UpdateUI(true);
+    }
+
+    void SqrtCalc()
+    {
+
+    }
+
+    void ExponentCalc()
+    {
+
+    }
+
+    void PercentCalc()
+    {
+
+    }
+    void EqualEquation()
+    {
+        //TODO Calucate final sum, 1 using left to right Order of Opperation, and 1 using BEDMAS Order of Opperation.
+    }
+
+    void ClearEverything()
+    {
+        //TODO Clear function.
     }
 
     void UpdateUI(bool bSum)
